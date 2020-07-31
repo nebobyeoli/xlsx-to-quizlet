2b70b902d2d1b1e464884104a9f0964cb2a8da30
NeboByeoli <nikkilover17@naver.com>
2020-07-31 17:43:50 +0900

    Created index.html

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const xlsx = require("xlsx");
 
+var log = document.getElementById("xlsx-drop").nodeValue;
+console.log(log);
+
 var workbook = xlsx.readFile("Day 15.xlsx");
 
 var worksheet = workbook.Sheets["Eng_meanings"];
