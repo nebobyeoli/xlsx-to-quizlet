713c1ecbb0d17ee238af2261d15756d14aa15206
NeboByeoli <nikkilover17@naver.com>
2020-08-08 22:32:21 +0900

    Testing usage of jekyll: Using github secrets in _config.yml

--- test-secret.js
+++ test-secret.js
@@ -4,4 +4,4 @@ console.log('process.env: ' + process.env);
 console.log('dotenv: ' + TEST_SECRET);
 */
 //console.log('github-secrets: ' + ${{secrets.TEST_SECRET}});
-console.log('github-secrets: ' + secrets.TEST_SECRET);
+console.log('github-secrets: ' + test_secret);
