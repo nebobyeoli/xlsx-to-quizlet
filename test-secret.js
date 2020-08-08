fb998dcbb6077c9f3b5cf7eb9e1c32f8fcaff38c
NeboByeoli <nikkilover17@naver.com>
2020-08-08 20:56:26 +0900

    Dotenv & github-secrets test

--- test-secret.js
+++ test-secret.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 console.log('process.env: ' + process.env);
 console.log('dotenv: ' + TEST_SECRET);
-console.log('github-secrets: ' + ${{secrets.TEST_SECRET}});
+//console.log('github-secrets: ' + ${{secrets.TEST_SECRET}});
 console.log('github-secrets: ' + secrets.TEST_SECRET);
