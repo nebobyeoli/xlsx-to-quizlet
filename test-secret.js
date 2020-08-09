b489c4b2d8514d95dfc81881be111de6a258f0fb
NeboByeoli <nikkilover17@naver.com>
2020-08-09 17:51:41 +0900

    Accessing env from Github Actions yml to javascript?

---
+++ test-secret.js
@@ -0,0 +1,10 @@
+/*
+import dotenv from 'dotenv';
+dotenv.config();
+require('dotenv').config();
+
+console.log('process.env: ' + process.env);
+console.log('dotenv: ' + TEST_SECRET);
+console.log('github-secrets: ' + ${{secrets.TEST_SECRET}});
+*/
+console.log('github-secrets: ' + process.env.test_secret);
