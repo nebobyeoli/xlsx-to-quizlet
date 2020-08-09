f0551336e65c03bd2e7a6a9492addc5cf4228e11
NeboByeoli <nikkilover17@naver.com>
2020-08-09 16:14:23 +0900

    Try: Nothing works...?

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
-const test_secret = process.env.test_secret;
+const webpack = require("webpack");
+
+//const test_secret = process.env.test_secret;
 
 module.exports = {
-    entry: "./script.js",
+    entry: "./test-secret.js",
     resolve: {
         extensions: ['.js']
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.test_secret': JSON.stringify(process.env.test_secret)
+        })
+    ]
 }
