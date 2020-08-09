64499c7d62af91a8469c34756faeb9e9864bcc00
NeboByeoli <nikkilover17@naver.com>
2020-08-09 14:14:57 +0900

    Added require('dotenv').config()

--- test-secret.js
+++ test-secret.js
@@ -1,7 +1,8 @@
-/*
 require('dotenv').config();
+/*
 console.log('process.env: ' + process.env);
 console.log('dotenv: ' + TEST_SECRET);
 */
 //console.log('github-secrets: ' + ${{secrets.TEST_SECRET}});
+
 console.log('github-secrets: ' + process.env.test_secret);
